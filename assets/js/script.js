6de76bfd1f259212d7627f7098ef76e2dfde0a5b
t.keta <keta37594@gmail.com>
2023-11-18 23:51:08 +0900

    jsをかいたよ

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#js-eventSlide',eventSlideOptions).mount();
+});
+
+
+const eventSlideOptions={
+    type :`loop`,
+    gap :20,
+    perPage:3,
+    pagination:true,
+    // focus:0,
+    }
+
+
+    new Splide('#js-eventSlide',eventSlideOptions).mount();
