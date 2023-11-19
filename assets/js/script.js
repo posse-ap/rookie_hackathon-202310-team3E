4b7b01b1c9598297edb999fedd78d6e223a7ce4e
Yuiito67 <yui.ito20040607@gmail.com>
2023-11-19 09:14:58 +0900

    おはよう

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,17 @@
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     new Splide('#js-eventSlide',eventSlideOptions).mount();
-// });
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#js-eventSlide',eventSlideOptions).mount();
+});
 
 
-// const eventSlideOptions={
-//     type :`loop`,
-//     gap :20,
-//     perPage:3,
-//     pagination:true,
-//     // focus:0,
-//     }
+const eventSlideOptions={
+    type :`loop`,
+    gap :20,
+    perPage:3,
+    pagination:true,
+    }
 
 
-//     new Splide('#js-eventSlide',eventSlideOptions).mount();
-
-//     // const tabs = document.querySelectorAll('[role="tab"]');
-//     // const tabList = document.querySelector('[role="tablist"]');
-    
-//     // const changeTabs = e => {
-//     //   const target = e.target;
-//     //   const selectedPanelId = target.getAttribute('aria-controls');
-//     //  // const grandparent = parent.parentNode;
-    
-//     //   // タブから現在すべての選択状態を取り除く
-//     //   parent
-//     //     .querySelectorAll('[aria-selected="true"]')
-//     //     .forEach(t => t.setAttribute("aria-selected", false));
-    
-//     //   // このタブを選択されたタブとして設定
-//     //   target.setAttribute("aria-selected", true);
-    
-//     //   // すべてのタブパネルを非表示
-//     //   grandparent
-//     //     .querySelectorAll('[role="tabpanel"]')
-//     //     .forEach(p => p.setAttribute("hidden", true));
-    
-//     //   // 選択されたパネルを表示
-//     //   grandparent
-//     //     .querySelector(`#${target.getAttribute("aria-controls")}`)
-//     //     .removeAttribute("hidden");
-//     // }
-    
-//     // tabs.forEach(tab => {
-//     //   tab.addEventListener('click', changeTabs);
-//     // });
-    
-//     const tabs = document.querySelectorAll('[role="tab"]');
-//     const panels = document.querySelectorAll('[role="tabpanel"]');
-    
-//     const changeTabs = e => {
-//       const target = e.target;
-//       const selectedPanelId = target.getAttribute('aria-controls');
-    
-//       // Remove selected state from all tabs
-//       tabs.forEach(tab => tab.setAttribute('aria-selected', false));
-    
-//       // Set selected state for the clicked tab
-//       target.setAttribute('aria-selected', true);
-    
-//       // Hide all panels
-//       panels.forEach(panel => panel.setAttribute('hidden', true));
-    
-//       // Show the selected panel
-//       document.getElementById(selectedPanelId).removeAttribute('hidden');
-//     };
-    
-//     tabs.forEach(tab => {
-//       tab.addEventListener('click', changeTabs);
-//     });
 
 window.addEventListener('load', () => {
   // checkbox
